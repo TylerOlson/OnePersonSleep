f379066602f5e1c7f99a43348487d1893b7240b0
Tyler Olson <tolson519@gmail.com>
2020-08-16 13:10:29 -0700

    Added an update checker

--- src/dev/tylerolson/onepersonsleep/OnePersonSleep.java
+++ src/dev/tylerolson/onepersonsleep/OnePersonSleep.java
@@ -2,6 +2,10 @@ package dev.tylerolson.onepersonsleep;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
 public class OnePersonSleep extends JavaPlugin {
 
     public static OnePersonSleep instance;
@@ -18,5 +22,29 @@ public class OnePersonSleep extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new SleepListener(), this);
 
         Metrics metrics = new Metrics(this, 8552);
+
+        String latestVersion = getLatestVersion();
+        if (latestVersion == "") {
+            getLogger().info("Could not get latest version!");
+        } else if (this.getDescription().getVersion().equals(latestVersion)) {
+            getLogger().info("Running latest version.");
+        } else if (!this.getDescription().getVersion().equals(latestVersion)) {
+            getLogger().info("Update available! Latest version is v" + latestVersion + ".");
+        }
     }
+
+    public String getLatestVersion() {
+        String latestVersion = "";
+        try {
+            URLConnection connection = new URL("https://api.spigotmc.org/legacy/update.php?resource=82804").openConnection();
+            Scanner scanner = new Scanner(connection.getInputStream());
+            latestVersion = scanner.next();
+            scanner.close();
+        } catch (Exception ex) {
+            getLogger().warning(ex.getMessage());
+        }
+
+        return latestVersion;
+    }
+
 }
